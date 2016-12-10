6acd5be3383e30fb75b5a8854ca52c7dec5205ea
zhayun <604863141@qq.com>
2016-12-10 22:56:56 +0800

    [修改][AcsServlet][去掉自动生成的注释]

--- JaveNotes/src/com/study/acs/AcsServlet.java
+++ JaveNotes/src/com/study/acs/AcsServlet.java
@@ -16,12 +16,8 @@ import javax.servlet.http.HttpServletResponse;
 public class AcsServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public AcsServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
     
     /* 
@@ -37,20 +33,12 @@ public class AcsServlet extends HttpServlet {
     	}
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		processRequest(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		processRequest(request, response);
 	}
 
