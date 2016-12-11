a7773da98fae7be228929eebee786e5d55499ac3
zhayun <604863141@qq.com>
2016-12-11 22:36:13 +0800

    [修改][servlet][分别输出http头部及body的内容]

--- JaveNotes/src/com/study/acs/AcsServlet.java
+++ JaveNotes/src/com/study/acs/AcsServlet.java
@@ -2,7 +2,7 @@ package com.study.acs;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-
+import java.util.Enumeration;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,32 +14,58 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/acs")
 public class AcsServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
        
     public AcsServlet() {
         super();
     }
-    
+ 
     /* 
      * 核心函数，处理ACS收到的GET/POST request
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    	//输出收到的内容
-    	BufferedReader br = request.getReader();
-    	String s=br.readLine();  
-    	while(null!=s){
-    		System.out.println(s);
-    		s=br.readLine();  
-    	}
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response){
+
+    	System.out.println("-------------------------------------------HTTP Header Information-------------------------------------------------------------------- ");
+    	 Enumeration em =request.getHeaderNames();//通过枚举类型获取请求文件的头部信息集
+    	 //遍历头部信息集
+    	 while(em.hasMoreElements()){          
+	    	//取出信息名                   
+	    	 String name=(String)em.nextElement();
+	    	//取出信息值              
+	    	 String value=request.getHeader(name);
+	    	  System.out.println(name+"="+value);
+    	 }
+    	 
+    	 System.out.println("-------------------------------------------HTTP SOAP Body Information-------------------------------------------------------------------- ");
+     	BufferedReader br = null;
+		try {
+			br = request.getReader();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+     	String s = null;
+		try {
+			s = br.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+		
+		//输出收到的内容
+     	while(null!=s){
+     		System.out.println(s);
+     		try {
+				s=br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}  
+     	}     
     }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response){
 		processRequest(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response){
 		processRequest(request, response);
 	}
-
 }
